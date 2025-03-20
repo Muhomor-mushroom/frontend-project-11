73514c599e722c6a57c40b8c08e4cabf68d3cd8f
AX <ax@MacBook-Pro-NS.local>
2025-03-20 17:21:33 +0300

    First step, first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import  './styles.scss';
+import  'bootstrap';
+import '/node_modules/bootstrap/dist/css/bootstrap.css.'
 console.log('Hello, World!');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   entry: './src/index.js',
@@ -28,7 +28,11 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
