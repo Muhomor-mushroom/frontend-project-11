316c73b640cab04ce53ce04e4392ff5b8bb9ca80
AX <ax@MacBook-Pro-NS.local>
2025-03-23 23:18:23 +0300

    First step, first commit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
-import "./styles.scss";
-import "bootstrap";
-import onChange from "on-change";
-import validate from "./yup.js";
-import view from "./view.js";
+import './styles.scss';
+import 'bootstrap';
+import onChange from 'on-change';
+import validate from './yup.js';
+import view from './view.js';
 
 /* ----------------------------SELECTORS--------------------------- */
 const elements = {
-  form: document.querySelector(".rss-form"),
-  input: document.querySelector("#url-input"),
+  form: document.querySelector('.rss-form'),
+  input: document.querySelector('#url-input'),
   confirmButton: document.querySelector('button[type="submit"]'),
 };
 /* ----------------------------INITIAL STATE-------------------------- */
 const initialState = {
   validationComplete: false,
-  errors: "",
+  errors: '',
 };
-const watchedState = onChange(initialState, (path, value, previous) => {
-    switch(path) {
-        case "activeUrl": 
-        if (watchedState.activeUrl === previous) {
-            watchedState.errors = {
-                "0": "Successfully used!",
-            }
-            view(elements, watchedState);
-        } else {
-          watchedState.errors = validate({ url: value });
-          view(elements, watchedState);
-        }
-        break;
-        case "errors":
-            break;
-        case "previousUrl":
-            break;
-        default:
-            console.log(`That's all!`)
-    }
+const watchedState = onChange(initialState, (path, value) => {
+  switch (path) {
+    case 'activeUrl':
+      watchedState.errors = validate({ url: value });
+      view(elements, watchedState);
+      break;
+    case 'errors':
+      view(elements, watchedState);
+      break;
+    case 'previousUrl':
+      break;
+    default:
+      console.log('That\'s all!');
+      break;
+  }
 });
 /* ----------------------------EVENT LISTENERS------------------------- */
-elements.form.addEventListener("submit", (e) => {
+elements.form.addEventListener('submit', (e) => {
   e.preventDefault();
   watchedState.previousUrl = watchedState.activeUrl;
   watchedState.activeUrl = elements.input.value;
+  if (watchedState.activeUrl === watchedState.previousUrl) {
+    watchedState.errors = { 0: 'Successfully Used!' };
+  }
   elements.form.reset();
 });
