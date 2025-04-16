c1647807b4e78c68d5faced7acf7f60403417d3f
AX <ax@MacBook-Pro-NS.local>
2025-04-16 18:52:49 +0300

    Seven step V 1.0

--- src/rssParser.js
+++ src/rssParser.js
@@ -2,9 +2,9 @@ const parseRss = (rss) => {
   const content = rss.data.contents;
   const parsedRss = new DOMParser(content);
   const dom = parsedRss.parseFromString(content, 'text/xml');
-  console.log(dom)
+  console.log(dom);
   /* eslint-disable */
-  if (dom.querySelector("parsererror") || !dom.querySelector('item')) {
+  if (dom.querySelector("parsererror") || !dom.querySelector('rss')) {
     throw new Error("notRss");
   }
   const resultObj = {
