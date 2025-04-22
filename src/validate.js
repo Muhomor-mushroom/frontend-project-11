aa3ad00485e7d5ad2a20e22a6f10f17cf463a23a
AX <ax@MacBook-Pro-NS.local>
2025-04-22 15:00:51 +0300

    Final step V 1.0

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 const validate = (url, urlsList) => {
+  /* --------------------------------- */
   const schema = yup.object().shape({
     url: yup.string().required().url().nullable()
       .notOneOf(urlsList),
